3e562d8032a6cdf033a3b21c6acfce8fb4035f5c
Rohan Bhagi <rohanbhagi99@gmail.com>
2024-05-04 20:45:47 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
-
-app.listen(3000,  () => {
-    console.log('application live on port 3000');
+const app = express()
+mongoose.connect(process.env.MONGODB).then(() => {
+    app.listen(3000,  () => {
+        console.log('application live on port 3000')
+    })
+}).catch((err) => {
+    console.log(err);
 });
+
