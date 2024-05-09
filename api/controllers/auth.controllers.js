951528a36dabf1cbac389efc3522f521b66a8947
Rohan Bhagi <rohanbhagi99@gmail.com>
2024-05-09 21:15:08 +0530

    Added sign in process that creates token and sends to UI

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { userName, email, password } = req.body;
@@ -8,6 +10,27 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json('User Created successfully!');
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user)  return next(errorHandler(404, 'User not found'));
+        const pass = bcrypt.compareSync(password, user.password);
+        if (!pass) return next(401, 'invalid credentials');
+        const token = jwt.sign({id: user._id }, process.env.JWT_SECRET);
+        console.log('singin true');
+        console.log(user._doc);
+        const { password: passkey, ...userDetails } = user._doc;
+        res
+            .cookie('token', token, { httpOnly: true  })
+            .status(200)
+            .json({ userDetails });
     }
     catch (error) {
         next(error);
